2a790751a68e001fead92e19164337b97dc5af5d
caioguimaraesr <caioguimaraesrocha70@gmail.com>
2024-12-26 14:26:00 -0300

    changes templates

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,4 +3,5 @@ from recipes import views
 
 urlpatterns = [
     path('', views.home), #/home
+    path('recipes/<int:id>/', views.recipes),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'recipes/pages/home.html')
+
+def recipes(request, id):
+    return render(request, 'recipes/pages/recipe-view.html')
