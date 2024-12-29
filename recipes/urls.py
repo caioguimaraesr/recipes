7b950dee17434ba456af4eb34a2992a940523933
caioguimaraesr <caioguimaraesrocha70@gmail.com>
2024-12-29 20:48:19 -0300

    change pages

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from recipes import views
 
+app_name = 'recipes'
+
 urlpatterns = [
-    path('', views.home), #/home
-    path('recipes/<int:id>/', views.recipes),
+    path('', views.home, name="home"), #/home
+    path('recipes/<int:id>/', views.recipes, name="recipe"),
 ]
