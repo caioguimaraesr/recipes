afbcc9d1d2adea14ce78ac2d9cb29e7de5cbdf57
caioguimaraesr <caioguimaraesrocha70@gmail.com>
2025-01-06 14:56:50 -0300

    create models

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+# CharField --> Campo de Texto (Curtos - Limitado)
+# IntegerField --> Número
+# TextField --> Campo de Texto (Longos - Sem Limite)
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=65) 
+    description = models.CharField(max_length=165)
+    slug = models.SlugField()
+    preparation_time = models.IntegerField()
+    preparation_time_unit = models.CharField(max_length=65)
+    servings = models.IntegerField()
+    servings_unit = models.CharField(max_length=65)
+    preparation_step = models.TextField()
+    preparation_step_is_html = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to='recipe/covers/%Y/%m/%d/')
--- recipes/urls.py
+++ recipes/urls.py
@@ -5,5 +5,5 @@ app_name = 'recipes'
 
 urlpatterns = [
     path('', views.home, name="home"), #/home
-    path('recipes/<int:id>/', views.recipes, name="recipe"),
+    path('recipes/<int:id>/', views.recipes, name="recipe"), #pagina de cada receita determinada pelo ID
 ]
