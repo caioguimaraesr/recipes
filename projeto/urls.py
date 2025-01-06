6faba7d137320971469c60d4d8f276457fe1c96f
caioguimaraesr <caioguimaraesrocha70@gmail.com>
2025-01-06 20:11:09 -0300

    config the static files

--- projeto/urls.py
+++ projeto/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('recipes.urls')), #puxa todas as urls da página views presente no app. 
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
