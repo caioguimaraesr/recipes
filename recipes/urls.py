72bfb92ca8c34e66d0aa073ff21492c47683eaaf
caioguimaraesr <caioguimaraesrocha70@gmail.com>
2025-01-09 21:49:29 -0300

    create url to category

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,5 +5,6 @@ app_name = 'recipes'
 
 urlpatterns = [
     path('', views.home, name="home"), #/home
+    path('recipes/category/<int:category_id>/', views.category, name='category'), 
     path('recipes/<int:id>/', views.recipes, name="recipe"), #pagina de cada receita determinada pelo ID
 ]
