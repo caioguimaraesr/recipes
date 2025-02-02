a9e59df60624d978a656d56c244b9ad7fe0d06ce
caioguimaraesr <caioguimaraesrocha70@gmail.com>
2025-02-02 18:21:00 -0300

    Change the mode that inserts the table data on the admin page.

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 from .models import Category, Recipe
 
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     ...
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     ...
-
-admin.site.register(Category, CategoryAdmin)
