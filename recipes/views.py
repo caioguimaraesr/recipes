d416418e75a04958e90bc8ade799cca60e403230
caioguimaraesr <caioguimaraesrocha70@gmail.com>
2025-02-02 13:51:48 -0300

    Delete get_list_or_404 for the page views.py

--- recipes/views.py
+++ recipes/views.py
@@ -1,24 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from utils.recipes.factory import make_recipe
 from recipes.models import Recipe
 from django.http import Http404, HttpResponse
 
 # Create your views here.
 def home(request):
-    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
-
-    if not recipes:
-        return render(request, 'recipes/pages/notFound.html', status=404)
-    
+    recipes = (
+        Recipe.objects.filter(
+            is_published=True,
+        ).order_by('-id')
+    )
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
-    
-    if not recipes:
-        return render(request, 'recipes/pages/notFound.html', status=404)
+    recipes =(
+        Recipe.objects.filter(
+            category__id=category_id, 
+            is_published=True,
+        ).order_by('-id')
+    )
     
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
