30b1653643f6452e2f6eb01cfb01a45f36512b79
caioguimaraesr <caioguimaraesrocha70@gmail.com>
2025-02-10 18:06:14 -0300

    creating a new functional test

--- tests/selenium_tests/recipes/test_recipe_home_page.py
+++ tests/selenium_tests/recipes/test_recipe_home_page.py
@@ -1,6 +1,8 @@
 from selenium.webdriver.common.by import By
 from .base import RecipeBaseFunctionalTest
+import pytest
 
+@pytest.mark.functional_test
 class RecipeHomePageFunctionaTest(RecipeBaseFunctionalTest):
     def test_recipe_home_page_without_recipes(self):
         self.browser.get(self.live_server_url)
